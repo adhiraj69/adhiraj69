794f8e22db8c7c4166e8c8264add75728741d95c
Adhiraj Banerjee <adhiraj@iitk.ac.in>
2023-12-15 22:01:46 +0530

    Update index.jsx

--- src/components/external-project/index.jsx
+++ src/components/external-project/index.jsx
@@ -142,7 +142,7 @@ const ExternalProject = ({ externalProjects, loading, googleAnalytics }) => {
                         skeleton({ width: 'w-40', height: 'h-8' })
                       ) : (
                         <span className="text-base-content opacity-70">
-                          Publications
+                          Publications and Talks
                         </span>
                       )}
                     </h5>
