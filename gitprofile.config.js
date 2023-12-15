597d8619aedf435d17c86c931ae3387f7f563599
Adhiraj Banerjee <adhiraj@iitk.ac.in>
2023-12-15 16:17:32 +0530

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -59,7 +59,7 @@ const config = {
     },
   ],
   // To hide the `My Projects` section, keep it empty.
-  publications: [
+  externalProjects: [
     {
       title: 'Project Name',
       description:
